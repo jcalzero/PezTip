803b026002693e24b04abc1677644d3dd17b99c8
Julien Calfayan <juliencalfayan@jailenes-macbook-pro.local>
2019-01-22 12:56:06 -0800

    Initial Commit

---
+++ PezTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PezTip
+//
+//  Created by Julien Calfayan on 1/22/19.
+//  Copyright © 2019 Julien Calfayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ PezTipTests/PezTipTests.swift
@@ -0,0 +1,34 @@
+//
+//  PezTipTests.swift
+//  PezTipTests
+//
+//  Created by Julien Calfayan on 1/22/19.
+//  Copyright © 2019 Julien Calfayan. All rights reserved.
+//
+
+import XCTest
+@testable import PezTip
+
+class PezTipTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
